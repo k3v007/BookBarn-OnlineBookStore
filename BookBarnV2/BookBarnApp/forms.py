fac33bb06e7b519c3f39839e15112683fa5e0340
Ankit <ankit2k48@gmail.com>
2018-04-20 20:10:29 +0530

    userData.txt and indentations

--- BookBarnV2/BookBarnApp/forms.py
+++ BookBarnV2/BookBarnApp/forms.py
@@ -2,7 +2,11 @@ from django import forms
 from django.contrib.auth.models import User
 from BookBarnApp.models import UserProfiles
 
+
+
+# User Signup Form Part 1
 class UserSignupForm(forms.ModelForm):
+
     first_name = forms.CharField(
             required=True, 
             max_length=50,
@@ -11,36 +15,85 @@ class UserSignupForm(forms.ModelForm):
                     attrs={
                         "class":"form-control", 
                         "placeholder":"First Name", 
-                        "id":"id_firstName"
+                        "id":"id_firstName",
                         }
                     )
             )
 
-    last_name = forms.CharField(required=True, max_length=50, label='', widget=forms.TextInput(
-                        attrs={
+    last_name = forms.CharField(
+            required=True,
+            max_length=50,
+            label='',
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"Last Name", 
                         "id":"id_lastName",
+                        }
+                    )
+            )
 
-                        }))
-
-    username = forms.CharField(required=True, min_length=6, max_length=50, label='', widget=forms.TextInput(
-                        attrs={
+    username = forms.CharField(
+            required=True,
+            min_length=6,
+            max_length=50,
+            label='',
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"Username", 
                         "id":"id_userName",
+                        }
+                    )
+            )
 
-                        }))
-    email = forms.EmailField(required=True, label='', widget=forms.EmailInput(attrs={ "class":"form-control", "placeholder":"Email", "id":"id_email"}))
-    password = forms.CharField(label='', widget=forms.PasswordInput( attrs={ "class":"form-control", "placeholder":"Password", "id":"id_password"}), required=True, min_length=6, max_length=50)
-    password2 = forms.CharField(label='', widget=forms.PasswordInput(attrs={ "class":"form-control", "placeholder":"Re-enter Password", "id":"id_password2"}), required=True, min_length=6, max_length=50)
+    email = forms.EmailField(
+            required=True,
+            label='',
+            widget=forms.EmailInput(
+                    attrs={
+                        "class":"form-control",
+                        "placeholder":"Email",
+                        "id":"id_email",
+                        }
+                    )
+            )
+
+    password = forms.CharField(
+            label='',
+            required=True,
+            min_length=6,
+            max_length=50,
+            widget=forms.PasswordInput(
+                    attrs={
+                        "class":"form-control",
+                        "placeholder":"Password",
+                        "id":"id_password",
+                        }
+                    )
+            )
+
+    password2 = forms.CharField(
+            label='',
+            required=True,
+            min_length=6,
+            max_length=50,
+            widget=forms.PasswordInput(
+                    attrs={
+                        "class":"form-control",
+                        "placeholder":"Re-enter Password",
+                        "id":"id_password2",
+                        }
+                    )
+            )
 
     def clean_email(self):
         email=self.cleaned_data.get("email")
         qs=User.objects.filter(email=email)
         if qs.exists():
             raise forms.ValidationError("Email already registered.")
-        return email
+        else:
+            return email
 
     def clean(self):
         data=self.cleaned_data
@@ -48,7 +101,7 @@ class UserSignupForm(forms.ModelForm):
         password2=self.cleaned_data.get("password2")
 
         if password2 != password:
-            raise forms.ValidationError("Passwords must match")
+            raise forms.ValidationError("Passwords must match.")
         else:
             return data
 
@@ -56,71 +109,143 @@ class UserSignupForm(forms.ModelForm):
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password')
 
+
+
+# User Signup Form Part 2
 class UserProfileSignupForm(forms.ModelForm):
-    phoneNumber = forms.CharField(required=True, label='', min_length=10, max_length=10, widget=forms.TextInput(
-                        attrs={
+
+    phoneNumber = forms.CharField(
+            required=True,
+            label='',
+            min_length=10,
+            max_length=10,
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"Phone Number", 
                         "id":"id_phoneNumber",
-                        }))                        
-    address1 = forms.CharField(required=True, label='', max_length=50, widget=forms.TextInput(
-                        attrs={
+                        }
+                    )
+            )
+
+    address1 = forms.CharField(
+            required=True,
+            label='',
+            max_length=50,
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"Address Line 1", 
                         "id":"id_address1",
-                        }))
-    address2 = forms.CharField(required=False, label='', max_length=50, widget=forms.TextInput(
-                        attrs={
+                        }
+                    )
+            )
+
+    address2 = forms.CharField(
+            required=False,
+            label='',
+            max_length=50,
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"Address Line 2", 
                         "id":"id_address2",
-                        }))
-    city = forms.CharField(required=True, label='', max_length=50, widget=forms.TextInput(
-                        attrs={
+                        }
+                    )
+            )
+
+    city = forms.CharField(
+            required=True, 
+            label='',
+            max_length=50,
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"City", 
                         "id":"id_city",
-                        }))
-    state = forms.CharField(required=True, label='', max_length=50, widget=forms.TextInput(
-                        attrs={
+                        }
+                    )
+            )
+
+    state = forms.CharField(
+            required=True,
+            label='',
+            max_length=50,
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"State", 
                         "id":"id_state",
-                        }))
-    # pinCode = forms.CharField(required=True, label='', min_length=6, max_length=6, widget=forms.TextInput(
-    #                     attrs={
+                        }
+                    )
+            )
+
+    # pinCode = forms.CharField(
+    #         required=True, 
+    #         label='', 
+    #         min_length=6, 
+    #         max_length=6, 
+    #         widget=forms.TextInput(
+    #                 attrs={
     #                     "class":"form-control", 
     #                     "placeholder":"PIN Code", 
     #                     "id":"id_pincode",
-    #                     }))
-    pinCode = forms.CharField(required=True, label='', min_length=6, max_length=6, widget=forms.TextInput(
-                        attrs={
+    #                     }
+    #                 )
+    #         )
+
+    pinCode = forms.CharField(
+            required=True, 
+            label='', 
+            min_length=6, 
+            max_length=6, 
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"PIN Code", 
                         "id":"id_pincode",
                         'pattern':'[0-9]+',
                         'autocomplete': 'off'
-                        }))
-
+                        }
+                    )
+            )
 
     class Meta:
         model = UserProfiles
         fields = ('address1', 'address2', 'city', 'state', 'pinCode', 'phoneNumber')
 
+
+
+
+# User Login Form
 class LoginForm(forms.ModelForm):
-    username = forms.CharField(required=True, label='', min_length=6, max_length=50, widget=forms.TextInput(
-                        attrs={
+    username = forms.CharField(
+            required=True, 
+            label='', 
+            min_length=6, 
+            max_length=50, 
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"Username", 
                         "id":"id_userName",
+                        }
+                    )
+            )
 
-                        }))
-    password = forms.CharField( label='', widget=forms.PasswordInput( attrs={
+    password = forms.CharField(
+            label='',
+            required=True, 
+            min_length=6, 
+            max_length=50,
+            widget=forms.PasswordInput( 
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"Password", 
                         "id":"id_password",
-
-                        } ), required=True, min_length=6, max_length=50)
+                        } 
+                    )
+            )
 
     class Meta:
         model = User
--- BookBarnV2/CartApp/forms.py
+++ BookBarnV2/CartApp/forms.py
@@ -5,103 +5,176 @@ ADDRESS_CHOICES = (
     ('other_add', "Other Address"),
 )
 
+# Address Selection Form
 class CheckoutForm(forms.Form):
-    delivery_address = forms.CharField(label='Select Delivery Address', widget=forms.RadioSelect(choices=ADDRESS_CHOICES))
 
+    delivery_address = forms.CharField(
+            label='Select Delivery Address',
+            widget=forms.RadioSelect(
+                    choices=ADDRESS_CHOICES,
+                    )
+            )
+
+# Form for filling New Address
 class NewAddressForm(forms.Form):                       
-    address1 = forms.CharField(required=True, label='', max_length=50, widget=forms.TextInput(
-                        attrs={
+    
+    address1 = forms.CharField(
+            required=True, 
+            label='', 
+            max_length=50, 
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"Address Line 1", 
                         "id":"id_address1",
-                        }))
-    address2 = forms.CharField(required=False, label='', max_length=50, widget=forms.TextInput(
-                        attrs={
+                        }
+                    )
+            )
+
+    address2 = forms.CharField(
+            required=False, 
+            label='', 
+            max_length=50, 
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"Address Line 2", 
                         "id":"id_address2",
-                        }))
-    city = forms.CharField(required=True, label='', max_length=50, widget=forms.TextInput(
-                        attrs={
+                        }
+                    )
+            )
+
+    city = forms.CharField(
+            required=True, 
+            label='', 
+            max_length=50, 
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"City", 
                         "id":"id_city",
-                        }))
-    state = forms.CharField(required=True, label='', max_length=50, widget=forms.TextInput(
-                        attrs={
+                        }
+                    )
+            )
+
+    state = forms.CharField(
+            required=True, 
+            label='', 
+            max_length=50, 
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"State", 
                         "id":"id_state",
-                        }))
-    pinCode = forms.CharField(required=True, label='', min_length=6, max_length=6, widget=forms.TextInput(
-                        attrs={
+                        }
+                    )
+            )
+
+    pinCode = forms.CharField(
+            required=True, 
+            label='', 
+            min_length=6, 
+            max_length=6, 
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"PIN Code", 
                         "id":"id_pincode",
                         'oninput': 'onlyNumber(id)'
-                        }))
-    phoneNumber = forms.CharField(required=True, label='', min_length=10, max_length=10, widget=forms.TextInput(
-                        attrs={
+                        }
+                    )
+            )
+
+    phoneNumber = forms.CharField(
+            required=True, 
+            label='', 
+            min_length=10, 
+            max_length=10, 
+            widget=forms.TextInput(
+                    attrs={
                         "class":"form-control", 
                         "placeholder":"Phone Number", 
                         "id":"id_phoneNumber",
                         'style': 'width:50ch',
                         'oninput': 'onlyNumber(id)'
-                        })) 
+                        }
+                    )
+            ) 
 
 
-
+# Form for crredit/debit card details
 class CardForm(forms.Form):
-    name = forms.CharField(
-        required=True, 
-        max_length=50,
-        label='', 
-        widget=forms.TextInput(
-                attrs={
-                    "class":"form-control", 
-                    "placeholder":"Name on Card", 
-                    "id":"name",
-                    "autocomplete":"off",
-                    }
-                )
-        )
-    
-    cvv = forms.CharField(label='', required=True, min_length=3, max_length=3, widget=forms.PasswordInput(
-        attrs={ 
-                "class":"form-control",
-                "placeholder":"CVV", 
-                "id":"cvv",
-                'oninput': 'onlyNumber(id)'
-            }   
-        )
-    )
-    
-    cardNumber = forms.CharField(required=True, label='', min_length=16, max_length=16, widget=forms.TextInput(
-            attrs={
-                "class":"form-control", 
-                "placeholder":"Card Number", 
-                "id":"cardNumber",
-                'oninput': 'onlyNumber(id)'
-            }
-        )
-    ) 
-    
-    date_mm = forms.CharField(required=True, label='', min_length=2, max_length=2, widget=forms.TextInput(
-            attrs={
-                "class":"form-control", 
-                "placeholder":"MM", 
-                "id":"date_mm",
-                'oninput': 'onlyNumber(id)'
-            }
-        )
-    )
 
-    date_yyyy = forms.CharField(required=True, label='', min_length=4, max_length=4, widget=forms.TextInput(
-            attrs={
-                "class":"form-control", 
-                "placeholder":"YYYY", 
-                "id":"date_yyyy",
-                'oninput': 'onlyNumber(id)'
-            }
-        )
-    )
+    name = forms.CharField(
+            required=True, 
+            max_length=50,
+            label='', 
+            widget=forms.TextInput(
+                    attrs={
+                        "class":"form-control", 
+                        "placeholder":"Name on Card", 
+                        "id":"name",
+                        "autocomplete":"off",
+                        }
+                    )
+            )
+        
+    cvv = forms.CharField(
+            label='', 
+            required=True, 
+            min_length=3, 
+            max_length=3, 
+            widget=forms.PasswordInput(
+                    attrs={ 
+                        "class":"form-control",
+                        "placeholder":"CVV", 
+                        "id":"cvv",
+                        'oninput': 'onlyNumber(id)',
+                        }   
+                    )
+            )
+    
+    cardNumber = forms.CharField(
+            required=True, 
+            label='', 
+            min_length=16, 
+            max_length=16, 
+            widget=forms.TextInput(
+                    attrs={
+                        "class":"form-control", 
+                        "placeholder":"Card Number", 
+                        "id":"cardNumber",
+                        'oninput': 'onlyNumber(id)',
+                        }
+                    )
+            ) 
+    
+    date_mm = forms.CharField(
+            required=True, 
+            label='', 
+            min_length=2, 
+            max_length=2, 
+            widget=forms.TextInput(
+                    attrs={
+                        "class":"form-control", 
+                        "placeholder":"MM", 
+                        "id":"date_mm",
+                        'oninput': 'onlyNumber(id)'
+                        }
+                    )
+            )
+
+    date_yyyy = forms.CharField(
+            required=True, 
+            label='', 
+            min_length=4, 
+            max_length=4, 
+            widget=forms.TextInput(
+                    attrs={
+                        "class":"form-control", 
+                        "placeholder":"YYYY", 
+                        "id":"date_yyyy",
+                        'oninput': 'onlyNumber(id)'
+                        }
+                    )
+            )
